e5f79704fc32a658ad2a0dca0614b24686a62061
eslamibrahim30 <eslam.ibrahim3082001@gmail.com>
2023-10-16 04:36:20 +0300

    Amenity

---
+++ models/amenity.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+This  classes that inherit from BaseModel.
+"""
+from models.base_model import BaseModel
+
+
+class Amenity(BaseModel):
+    '''
+class inherit from BaseModel.
+'''
+    name = ""
+
