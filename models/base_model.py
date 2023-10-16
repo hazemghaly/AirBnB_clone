c869a432e6f8e6371bcc716fd9ee133411463405
hazemghaly <hazemghaly30@gmail.com>
2023-10-15 18:38:27 -0700

    files with codes

---
+++ models/base_model.py
@@ -0,0 +1,64 @@
+#!/usr/bin/python3
+"""
+This module contains the BaseModel class.
+"""
+import uuid
+import datetime
+from models import storage
+
+
+class BaseModel:
+    """
+This class will be the 'base' of all other classes in this project.
+The goal of it is to manage id attribute in all future classes and
+to avoid duplicating the same code (by extension, same bugs).
+"""
+    def __init__(self, *args, **kwargs):
+        """
+        This method initializes a new created BaseModel object.
+        """
+        if (kwargs is None or len(kwargs) == 0):
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.datetime.now()
+            self.updated_at = datetime.datetime.now()
+            storage.new(self)
+        else:
+            for key in kwargs:
+                if key == '__class__':
+                    pass
+                elif key == 'created_at' or key == 'updated_at':
+                    self.__dict__[key] = datetime.datetime.fromisoformat(
+                        kwargs[key])
+                else:
+                    self.__dict__[key] = kwargs[key]
+
+    def __str__(self):
+        """
+    This class method provides the informal string representation of BaseModel.
+        """
+        my_str = "[{}] ({}) {}".format(
+                        self.__class__.__name__,
+                        self.id,
+                        self.__dict__)
+        return my_str
+
+    def save(self):
+        """
+This public instance method updates the public instance
+attribute updated_at with the current datetime.
+    """
+        self.updated_at = datetime.datetime.now()
+        storage.new(self)
+        storage.save()
+
+    def to_dict(self):
+        """
+    This public instance method returns returns a dictionary
+    containing all keys/values of __dict__ of the instance.
+        """
+        my_dict = {}
+        my_dict.update(self.__dict__)
+        my_dict['__class__'] = self.__class__.__name__
+        my_dict['created_at'] = self.created_at.isoformat()
+        my_dict['updated_at'] = self.updated_at.isoformat()
+        return my_dict
