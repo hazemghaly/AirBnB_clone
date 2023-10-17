635b82010d346522694eb4c9695a7e5e01872f97
eslamibrahim30 <eslam.ibrahim3082001@gmail.com>
2023-10-17 15:22:58 +0300

    Test BaseModel

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -1,1 +1,34 @@
 #!/usr/bin/python3
+"""
+Unittest for BaseModel class.
+"""
+import unittest
+import datetime
+from models.base_model import BaseModel
+
+
+class TestBase(unittest.TestCase):
+    """
+    This class for testing a BaseModel class.
+    """
+
+    def testBaseModelSave(self):
+        b = BaseModel()
+        b.save()
+
+    def testBaseModelToDict(self):
+        b = BaseModel()
+        my_dict = b.to_dict()
+
+    def testBaseModelDifferentID(self):
+        b1 = BaseModel()
+        b2 = BaseModel()
+        self.assertNotEqual(b1.id, b2.id)
+
+    def testBaseModelCreatedAt(self):
+        b = BaseModel()
+        self.assertIsInstance(b.created_at, datetime.datetime)
+
+    def testBaseModelStr(self):
+        b = BaseModel()
+        self.assertIsInstance(b.__str__(), str)
