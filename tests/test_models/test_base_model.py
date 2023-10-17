827137faf0a355f4b02823eea16c9335ed6eb930
eslamibrahim30 <eslam.ibrahim3082001@gmail.com>
2023-10-17 15:43:10 +0300

    Test BaseModel

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -5,6 +5,7 @@ Unittest for BaseModel class.
 import unittest
 import datetime
 from models.base_model import BaseModel
+from models import storage
 
 
 class TestBase(unittest.TestCase):
@@ -14,11 +15,19 @@ class TestBase(unittest.TestCase):
 
     def testBaseModelSave(self):
         b = BaseModel()
+        updated_time_1 = b.updated_at
         b.save()
+        updated_time_2 = b.updated_at
+        self.assertNotEqual(updated_time_1, updated_time_2)
 
     def testBaseModelToDict(self):
         b = BaseModel()
         my_dict = b.to_dict()
+        self.assertIsInstance(my_dict, dict)
+        self.assertEqual(b.id, my_dict['id'])
+        self.assertEqual(b.created_at.isoformat(), my_dict['created_at'])
+        self.assertEqual(b.updated_at.isoformat(), my_dict['updated_at'])
+        self.assertEqual(b.__class__.__name__, my_dict['__class__'])
 
     def testBaseModelDifferentID(self):
         b1 = BaseModel()
@@ -31,4 +40,9 @@ class TestBase(unittest.TestCase):
 
     def testBaseModelStr(self):
         b = BaseModel()
+        my_str = "[{}] ({}) {}".format(
+                        b.__class__.__name__,
+                        b.id,
+                        b.__dict__)
         self.assertIsInstance(b.__str__(), str)
+        self.assertEqual(b.__str__(), my_str)
